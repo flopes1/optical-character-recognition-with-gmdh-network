50fea4697094a554d496e4ae3769ab72bb5b6360
Filipe Lopes <flm@ecomp.poli.br>
2017-06-23 18:01:29 -0300

    Atualiza referencias pós merge

---
+++ OCRFFNetwork/OCRFFNetwork/model/DoubleRange.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCRFFNetwork.model
+{
+	public class DoubleRange
+	{
+		private double min, max;
+
+		public double Min
+		{
+			get
+			{
+				return min;
+			}
+
+			set
+			{
+				if (value != min)
+				{
+					min = value;
+				}
+			}
+		}
+
+		public double Max
+		{
+			get
+			{
+				return max;
+			}
+
+			set
+			{
+				if (value != max)
+				{
+					max = value;
+				}
+			}
+		}
+
+		public double Length
+		{
+			get { return max - min; }
+		}
+
+		public DoubleRange(double min, double max)
+		{
+			this.min = min;
+			this.max = max;
+		}
+	}
+}
---
+++ OCRFFNetwork/OCRFFNetwork/model/Layer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace OCRFFNetwork.model
+{
+    public class Layer
+    {
+		protected Layer (int neuronsCount, int inputsCount, SigmoidFunction sigmoid)
+		{
+			for (int i = 0; i < neuronsCount; i++)
+				_neuronList[i] = new Neuron(inputsCount, sigmoid);
+		}
+
+		public ObservableCollection<Double> Compute(double[] input)
+		{
+			// compute each neuron
+			for (int i = 0; i < _neuronList.Count; i++)
+				_outputValues[i] = _neuronList[i].Compute(input);
+
+			return _outputValues;
+		}
+
+
+		private ObservableCollection<Neuron> _neuronList = new ObservableCollection<Neuron>();
+
+		public ObservableCollection<Neuron> NeuronList
+		{
+			get
+			{
+				return _neuronList;
+			}
+
+			set
+			{
+				if (value != _neuronList)
+				{
+					_neuronList = value;
+				}
+			}
+		}
+
+		public ObservableCollection<Double> _outputValues = new ObservableCollection<Double>();
+
+		public ObservableCollection<Double> OutputValues
+		{
+			get
+			{
+				return _outputValues;
+			}
+		}
+	}
+}
