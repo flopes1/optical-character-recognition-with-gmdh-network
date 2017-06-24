83d252cfb087b88e1abda1adf7d83ed48c400bf1
Filipe Lopes <flm@ecomp.poli.br>
2017-06-24 18:32:08 -0300

    Adiciona interface dos passos da rede mlp

---
+++ OCRFFNetwork/OCRFFNetwork/model/INetwork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCRFFNetwork.model
+{
+    public interface INetwork
+    {
+        ObservableCollection<double> ForwardStep(Example currentExample);
+        void TrainNetwork();
+        void UpdateNetworkWeights();
+        bool CheckElement(Example elemtToTest);
+        void SaveCurrentWeights();
+        void BackwardSted(Example currentExample);
+    }
+}
