3799249f216e9cfafdfd609186c51acd1de85288
Filipe Lopes <flm@ecomp.poli.br>
2017-06-23 21:12:50 -0300

    Move classe para pacote correto

--- OCRFFNetwork/OCRFFNetwork/api/MLPNetwork.cs
+++ OCRFFNetwork/OCRFFNetwork/api/MLPNetwork.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
 
-namespace OCRFFNetwork.model
+namespace OCRFFNetwork.api
 {
 	// TODO
     public class MLPNetwork
