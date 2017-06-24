e1fda0a2172aff6ccc5bd8ba501c018935ea7381
Filipe Lopes <flm@ecomp.poli.br>
2017-06-24 18:32:56 -0300

    Adiciona classe ciclo

---
+++ OCRFFNetwork/OCRFFNetwork/model/Cycle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCRFFNetwork.model
+{
+    public class Cycle
+    {
+
+        public Cycle()
+        {
+            this.Examples = new ObservableCollection<Example>();
+        }
+
+        public ObservableCollection<Example> Examples { get; set; }
+    }
+}
