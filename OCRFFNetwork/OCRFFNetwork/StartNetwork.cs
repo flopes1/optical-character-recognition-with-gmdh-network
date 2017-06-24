a4a7eca9449dc46ac0ed0720241c9a4624a59923
Filipe Lopes <flm@ecomp.poli.br>
2017-06-24 18:34:51 -0300

    Adiciona classe de teste

--- OCRFFNetwork/OCRFFNetwork/StartNetwork.cs
+++ OCRFFNetwork/OCRFFNetwork/StartNetwork.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using OCRFFNetwork.model;
 using OCRFFNetwork.dataset;
+using OCRFFNetwork.api.Utils;
 
 namespace OCRFFNetwork
 {
@@ -14,6 +15,15 @@ namespace OCRFFNetwork
     {
         public static void Main(String[] args)
         {
+
+            var trainAlphabet = new AlphabetDataset(EnumDatasetType.Train);
+            var validationAlphabet = new AlphabetDataset(EnumDatasetType.Validation);
+            var testAlphabet = new AlphabetDataset(EnumDatasetType.Test);
+
+            var cyclesToTrain = DatasetUtils.BuildCyclesFromDataset(trainAlphabet);
+            var netWork = new MultiLayerNetwork(cyclesToTrain);
+
+
             //var input = "C:\\Users\\Filipe Lopes\\Desktop\\8º Periodo\\RNA\\Dataset\\Maiusculas";
             //var output = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName + "\\dataset";
 
@@ -25,8 +35,41 @@ namespace OCRFFNetwork
             //var dataSet = new AlphabetDataset(EnumDatasetType.Train);
             //dataSet.Alphabet.FirstOrDefault().GetImagePixels(0);
 
+            //var example = new Example()
+            //{
+            //    Name = "A",
+            //    InputValues = new ObservableCollection<double>(new double[] { 1, 0 }),
+            //    WantedValues = new ObservableCollection<double>(new double[] { 0, 0 })
+            //};
+
+
+            //var inputLayer = new Layer(1, 2, 0, new SigmoidFunction());
+            //var hidLayer = new Layer(2, 3, inputLayer.Neurons.Count, new SigmoidFunction());
+            //var outputLayer = new Layer(3, 2, hidLayer.Neurons.Count, new SigmoidFunction(), true);
+
+            //var netWorkLayers = new ObservableCollection<Layer>();
+            //netWorkLayers.Add(inputLayer);
+            //netWorkLayers.Add(hidLayer);
+            //netWorkLayers.Add(outputLayer);
+
+            //var previousLayerOutput = new ObservableCollection<double>();
+
+            //foreach (var layer in netWorkLayers)
+            //{
+            //    if (layer.IsFirstLayer)
+            //    {
+            //        layer.CalculateLayerOutput(example.InputValues);
+            //    }
+            //    else
+            //    {
+            //        layer.CalculateLayerOutput(previousLayerOutput);
+            //    }
+            //    previousLayerOutput = layer.OutputValues;
+            //}
 
         }
+
+        
 
     }
 }
